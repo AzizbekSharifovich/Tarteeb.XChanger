a89ecac2f821d122df5d3a610c241081acbed534
AzizbekSharifovich <azizbek0698@gmail.com>
2023-10-13 11:39:13 +0500

    ORCHESTRATIONS: Import Applicant (WIP)

---
+++ Tarteeb.XChanger/Services/Foundations/ProccesingService/SpreadsheetProccesingService.cs
@@ -0,0 +1,18 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System.Collections.Generic;
+using System.IO;
+using Tarteeb.XChanger.Models;
+
+namespace Tarteeb.XChanger.Services.Foundations.ProccesingService;
+public class SpreadsheetProccesingService : ISpreadsheetProccesingService
+{
+    public List<ExternalApplicant> GetExternalApplicants(MemoryStream memoryStream)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
--- Tarteeb.XChanger/Services/Orchestrations/OrchestrationService.cs
+++ Tarteeb.XChanger/Services/Orchestrations/OrchestrationService.cs
@@ -3,14 +3,28 @@
 // Powering True Leadership
 //=================================
 
+using System.Collections.Generic;
 using System.IO;
+using Tarteeb.XChanger.Models;
+using Tarteeb.XChanger.Services.Foundations.ProccesingService;
+using Tarteeb.XChanger.Services.Orchestrations.Interfaces;
 
 namespace Tarteeb.XChanger.Services.Orchestrations;
 
-public class OrchestrationService
+public class OrchestrationService : IOrchestrationService
 {
+    List<ExternalApplicant> validExternalApplicants;
+    private readonly ISpreadsheetProccesingService spreadsheetProccesingService;
+
+    public OrchestrationService(ISpreadsheetProccesingService spreadsheetProccesingService)
+    {
+        validExternalApplicants = new List<ExternalApplicant>();
+        this.spreadsheetProccesingService = spreadsheetProccesingService;
+
+    }
+
     public void ProccesingImportRequest(MemoryStream stream)
     {
-
+        this.validExternalApplicants = this.spreadsheetProccesingService.GetExternalApplicants(stream);
     }
 }
