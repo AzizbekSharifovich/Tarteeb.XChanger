f3c2e467435288a94ea831582da6faeced215b54
AzizbekSharifovich <azizbek0698@gmail.com>
2023-10-14 18:54:48 +0500

    OK

---
+++ Tarteeb.XChanger/Services/ISpreadsheetService.cs
@@ -0,0 +1,11 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+namespace Tarteeb.XChanger.Services;
+
+public interface ISpreadsheetService
+{
+          
+}
