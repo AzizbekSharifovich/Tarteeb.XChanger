08c181ba7a7fc9e6bb5766b84b33bc0b2da5399f
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-10-14 10:36:51 +0500

    POC:  import applicant

--- Tarteeb.XChanger/Services/Foundations/ProccesingService/SpreadsheetProccesingService.cs
+++ Tarteeb.XChanger/Services/Foundations/ProccesingService/SpreadsheetProccesingService.cs
@@ -5,14 +5,20 @@
 
 using System.Collections.Generic;
 using System.IO;
+using Tarteeb.XChanger.Brokers;
 using Tarteeb.XChanger.Models;
 
 namespace Tarteeb.XChanger.Services.Foundations.ProccesingService;
 public class SpreadsheetProccesingService : ISpreadsheetProccesingService
 {
+    private readonly ISpreadSheetBroker spreadSheetBroker;
+    public SpreadsheetProccesingService(ISpreadSheetBroker spreadSheetBroker)
+    {
+        this.spreadSheetBroker = spreadSheetBroker;
+    }
     public List<ExternalApplicant> GetExternalApplicants(MemoryStream memoryStream)
     {
-        throw new System.NotImplementedException();
+        return spreadSheetBroker.ReadExternalApplicants(memoryStream);
     }
 }
 
