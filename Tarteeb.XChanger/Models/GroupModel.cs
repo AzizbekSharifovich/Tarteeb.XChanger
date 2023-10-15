7bd8831f363b2148467b6bf86071155a442098c5
AzizbekSharifovich <azizbek0698@gmail.com>
2023-10-15 17:18:50 +0500

    MODELS: Add Header File

---
+++ Tarteeb.XChanger/Models/GroupModel.cs
@@ -0,0 +1,14 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System;
+
+namespace Tarteeb.XChanger.Models;
+
+public class GroupModel
+{
+    public string GroupName { get; set; }
+    public Guid GroupId { get; set; }
+}
