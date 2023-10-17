7b9f5075eea21265c6e5a93c1563fe763a74b0ab
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-10-17 20:41:40 +0500

    DATA: Create & Migrate ExternalApplicant

---
+++ Tarteeb.XChanger/Migrations/20231017153826_addDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tarteeb.XChanger.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
