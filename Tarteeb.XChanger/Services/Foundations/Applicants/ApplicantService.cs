b1521b89c5e15f11d132f482ca86a78cea390e33
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-10-19 12:25:31 +0500

    SERVICES: Add Applicant

---
+++ Tarteeb.XChanger/Services/Foundations/Applicants/ApplicantService.cs
@@ -0,0 +1,18 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System.Threading.Tasks;
+using Tarteeb.XChanger.Models;
+
+namespace Tarteeb.XChanger.Services.Foundations.Applicants
+{
+    public class ApplicantService : IApplicantService
+    {
+        public ValueTask<ExternalApplicantModel> InsertApplicantAsync(ExternalApplicantModel externalApplicantModel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Tarteeb.XChanger/Services/Foundations/Applicants/IApplicantService.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System.Threading.Tasks;
+using Tarteeb.XChanger.Models;
+
+namespace Tarteeb.XChanger.Services.Foundations.Applicants
+{
+    public interface IApplicantService
+    {
+        ValueTask<ExternalApplicantModel> InsertApplicantAsync(ExternalApplicantModel externalApplicantModel);
+    }
+}
