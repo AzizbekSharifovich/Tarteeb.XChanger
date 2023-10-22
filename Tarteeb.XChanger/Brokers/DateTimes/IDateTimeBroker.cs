a23bf92bd2e506d12212aa8f6e2ccc3ef8f2c406
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-10-22 10:29:55 +0500

    BROKERS: Add DateTimeBroker

---
+++ Tarteeb.XChanger/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//=================================
+
+using System;
+
+namespace Tarteeb.XChanger.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
